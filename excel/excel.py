1598f598f623e740dbd66dcd796c100809ee52da
Amateur221 <ly6624@163.com>
2019-04-23 15:04:41 +0800

    20190423
    获取前八行数据

--- excel/excel.py
+++ excel/excel.py
@@ -9,18 +9,32 @@ nCols = table.ncols
 print("%d" % nRows)
 print("%d" % nCols)
 
-rows1 = table.row_values(0)
-print("%s" % rows1)
-#cell_A1 = table.cell_value(0, 0)
-#print("%s" % cell_A1)
+# 读取Excel表格前七行数据并存储在列表rowsTable
+rowsTable = []
+for num in range(8):
+    rowsTable.append(table.row_values(num))
+
+print("%s" % rowsTable)
+# cell_A1 = table.cell_value(0, 0)
+# print("%s" % cell_A1)
 
 
 
-workbook = xlwt.Workbook(encoding='utf-8', style_compression=0)
-sheet = workbook.add_sheet('test', cell_overwrite_ok=True)
-#sheet.write(0, 0, cell_A1)
-for cell_value1 in rows1:
-    sheet.write(0, 0, cell_value1)
-    print("%s" % cell_value1)
+workBook = xlwt.Workbook(encoding = 'utf-8', style_compression = 0)
+sheet = workBook.add_sheet('test', cell_overwrite_ok=True)
+# sheet.write(0, 0, cell_A1)
 
-workbook.save(r"D:\GitHub\python\1.xls")
+for num1 in range(8):
+    ColsNum = 0
+    while ColsNum < nCols:
+        for cell_value1 in rowsTable[num1]:
+            sheet.write(num1, ColsNum, cell_value1)
+            if len(rowsTable[num1][ColsNum]) != 0:
+                ColsNum += 1
+                print("%d" % ColsNum)
+            else:
+                ColsNum = 27
+                break
+            print("%s" % cell_value1)
+
+workBook.save(r"D:\GitHub\python\2.xls")
